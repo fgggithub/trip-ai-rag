6223f2a235cc71af956a3fc686c0f18275e5e976
Fred Graichen <fredgraichen@outlook.com>
2025-03-06 15:03:40 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+## Use Generative AI and Next.js with AWS Amplify to build a Fullstack Recipe Generator
+
+Let's dive into the world of Generative AI, Next.js, AWS Amplify, and Amazon Bedrock supercharged by Claude 3. This is an example shows how to build a recipe generator app where users can input a list of ingredients, and Claude 3 will generate delicious recipes based on their selection.
+
+![recipe-generator](images/recipe-generator.gif)
+
+## Getting Started
+### Clone repo
+
+```
+
+git clone https://github.com/aws-samples/recipe-ai.git
+cd recipe-ai
+
+```
+
+### Install the packages
+
+```
+
+npm i
+
+```
+
+### Initiate a cloud sandbox environment
+
+```
+
+npx ampx sandbox
+
+```
+
+### Run the App
+
+```
+
+npm run dev
+
+```
+
+
+
+## Security
+
+See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
+
+## License
+
+This library is licensed under the MIT-0 License. See the LICENSE file.
+
---
+++ amplify/data/resource.ts
@@ -0,0 +1,42 @@
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+
+const schema = a.schema({
+  BedrockResponse: a.customType({
+    body: a.string(),
+    error: a.string(),
+  }),
+
+  askBedrock: a
+    .query()
+    .arguments({ ingredients: a.string().array() })
+    .returns(a.ref("BedrockResponse"))
+    .authorization(allow => allow.publicApiKey())
+    .handler(
+      a.handler.custom({ entry: "./bedrock.js", dataSource: "bedrockDS" })
+    ),
+
+  knowledgeBase: a
+    .query()
+    .arguments({ input: a.string() })
+    .handler(
+      a.handler.custom({
+        dataSource: "KnowledgeBaseDataSource",
+        entry: "./kbResolver.js",
+      })
+    )
+    .returns(a.string())
+    .authorization((allow) => [allow.authenticated()]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "apiKey",
+    // API Key is used for a.allow.public() rules
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
+  },
+});
