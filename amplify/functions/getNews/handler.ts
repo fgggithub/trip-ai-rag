d78787f6d78fd7054f6bd35cc3de96a3479f81d6
Fred Graichen <fredgraichen@outlook.com>
2025-04-03 12:09:23 -0500

    updated news api logic

--- amplify/functions/getNews/handler.ts
+++ amplify/functions/getNews/handler.ts
@@ -11,7 +11,7 @@ export const handler: Schema["getNews"]["functionHandler"] = async (event) => {
   //);
 
   const res = await fetch(
-    `${URL}${encodeURIComponent(event.arguments.category ?? "")}&apiKey='a148e525ae6d4da4bddccf1a8a810154'`
+    `${URL}${encodeURIComponent(event.arguments.category ?? "")}&apiKey=a148e525ae6d4da4bddccf1a8a810154`
   );
   console.log("event", event);
 
