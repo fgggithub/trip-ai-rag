aa02a4a5a02194688d8519af0e1443bb7a1f2a08
Fred Graichen <fredgraichen@outlook.com>
2025-04-14 11:44:34 -0500

    added trip planner

---
+++ amplify/functions/tripplanner/handler.ts
@@ -0,0 +1,51 @@
+
+import {
+  BedrockAgentRuntimeClient,
+  InvokeAgentCommand
+} from "@aws-sdk/client-bedrock-agent-runtime";
+import type { Schema } from "../../data/resource";
+//import { env } from "$amplify/env/tripplanner";
+const AGENT_ID = "NRZCOEQP33";
+const AGENT_ALIAS_ID = "SDZYRHCISE";
+const REGION = "us-west-2";
+
+export const handler: Schema["readKnowledgebase"]["functionHandler"] = async (event) => {
+  const inputText = String(event.arguments.prompt);
+
+  const client = new BedrockAgentRuntimeClient({ region: "us-west-2" });
+
+  const input = {
+    agentId: AGENT_ID,
+    agentAliasId: AGENT_ALIAS_ID,
+    sessionId: "session-001",
+    inputText,
+    enableTrace: true
+  };
+
+  try {
+    const command = new InvokeAgentCommand(input);
+    const response = await client.send(command);
+
+    let resultText = "";
+
+    if (response.completion) {
+      for await (const event of response.completion) {
+        if (event.chunk?.bytes) {
+          resultText += new TextDecoder().decode(event.chunk.bytes);
+        }
+      }
+    } else {
+      resultText = "No completion response from the agent.";
+    }
+
+    return {
+      resultText
+    };
+  } catch (error) {
+    console.error("Error invoking agent:", error);
+
+    return {
+      resultText: "An error occurred while invoking the agent."
+    };
+  }
+};
