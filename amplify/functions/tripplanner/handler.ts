5a3688208fca5f195e5ded2e1569e6c61a5defb4
Fred Graichen <fredgraichen@outlook.com>
2025-04-14 12:36:14 -0500

    added trip planner console logs

--- amplify/functions/tripplanner/handler.ts
+++ amplify/functions/tripplanner/handler.ts
@@ -21,18 +21,19 @@ export const handler: Schema["tripplanner"]["functionHandler"] = async (event) =
     inputText,
     enableTrace: true
   };
-
+  console.log("input", input);
   try {
     const command = new InvokeAgentCommand(input);
     const response = await client.send(command);
 
     let resultText = "";
-
+    console.log("input", response);
     if (response.completion) {
       for await (const event of response.completion) {
         if (event.chunk?.bytes) {
           resultText += new TextDecoder().decode(event.chunk.bytes);
         }
+        console.log("result text", resultText);
       }
     } else {
       resultText = "No completion response from the agent.";
